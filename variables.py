9f242b77b7534c7e3fe849392d69fa4ad92e96ae
Justin Potts <justinpotts98@gmail.com>
2015-01-05 20:33:42 -0600

    Updated variables file to only include necessary settings

--- variables.py
+++ variables.py
@@ -6,10 +6,6 @@
 # This will be used to grab data from http://www.weather.com
 zip = 75070
 
-# Time zone - for use as a default setup variable. This can be changed in
-# admin settings while logged in, but will be lost on shut down.
-time_zone = 'CST'
-
 # Unit of measurement - Either metric or imperial can be chosen. This can be
 # changed in admin settings while logged in, but will be lost on shut down.
 unit = 'imperial'
